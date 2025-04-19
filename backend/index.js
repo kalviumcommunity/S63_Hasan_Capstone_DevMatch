5fcf9115805c89d88553e859b4adf9c355717901
Hasan <hasan@Hasans-MacBook-Air.local>
2025-04-19 23:49:51 +0530

    auth

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("✅ MongoDB Connected Successfully!"))
@@ -12,6 +13,7 @@ mongoose.connect(process.env.MONGO_URI)
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.use(cors());
 app.use(express.json());
 
 // Import route files
@@ -19,12 +21,14 @@ const userRouter = require("./routes/userRouter");
 const profileRouter = require("./routes/profileRouter");
 const requestRouter = require("./routes/requestRouter");
 const uploadRouter = require("./routes/uploadRouter");
+const authRouter = require("./routes/auth");
 
 // Use routers
 app.use("/api/requests", requestRouter);
 app.use("/api/profiles", profileRouter);
 app.use("/api/users", userRouter);
 app.use("/api/upload", uploadRouter);
+app.use("/api/auth", authRouter);
 
 // Root route
 app.get('/', (req, res) => {
